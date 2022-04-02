8bf2d7c4805a82ea052ce94e298b8f2494a3f1cf
David Siddle <siddav83@gmail.com>
2022-04-02 22:30:08 +0100

    another component added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Bookings from "./Bookings";
 import Header from "./Header";
 import "./App.css";
+import TouristInfoCards from "./TouristInfoCards";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <TouristInfoCards />
       <Bookings />
     </div>
   );
