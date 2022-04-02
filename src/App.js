6ba62f2baab9d7e66951efeeef1c5b4e1a2e8251
David Siddle <siddav83@gmail.com>
2022-04-02 22:19:59 +0100

    moved component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-
 import Bookings from "./Bookings";
+import Header from "./Header";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <Header />
       <Bookings />
     </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="App-header">
+      <img />
+      CYF Hotel
+    </header>
+  );
+};
+
+export default Header;
